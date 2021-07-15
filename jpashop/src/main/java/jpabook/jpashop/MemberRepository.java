1afbdcd21981193e1965ea0ac754d2ff53399006
yukuda <leeyookoo@naver.com>
2021-07-15 16:20:34 +0900

    JPA

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);     //persist: 엔티티를 영속화 한다는 뜻 db저장은 그 이후
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
